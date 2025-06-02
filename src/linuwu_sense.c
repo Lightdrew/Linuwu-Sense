fa5daae7f76d028887cc6c0def074876b28daf21
Lightdrew <light02h18@gmail.com>
2025-06-02 19:20:17 +0200

    add an16-51 profile

--- src/linuwu_sense.c
+++ src/linuwu_sense.c
@@ -485,7 +485,12 @@ enum acer_wmi_predator_v4_oc {
     .four_zone_kb = 1,
  };
 
-  static struct quirk_entry quirk_acer_nitro_an16_43 = {
+ static struct quirk_entry quirk_acer_nitro_an16_51 = {
+    .nitro_v4 = 1,
+    .four_zone_kb = 1,
+ };
+
+ static struct quirk_entry quirk_acer_nitro_an16_43 = {
     .nitro_v4 = 1,
     .four_zone_kb = 1,
  };
@@ -568,7 +573,7 @@ enum acer_wmi_predator_v4_oc {
              DMI_MATCH(DMI_SYS_VENDOR, "Acer"),
              DMI_MATCH(DMI_PRODUCT_NAME, "Nitro AN16-51"),
          },
-         .driver_data = &quirk_acer_predator_phn16_72,
+         .driver_data = &quirk_acer_nitro_an16_51,
      },
      {
          .ident = "Acer Nitro AN16-43",
